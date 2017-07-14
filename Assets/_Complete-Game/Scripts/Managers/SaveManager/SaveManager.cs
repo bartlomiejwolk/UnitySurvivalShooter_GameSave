c6745b909d6e1cd5110aa0bbaea46aaa497a6972
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2017-07-14 09:43:06 +0200

    Serialize mock data

--- Assets/_Complete-Game/Scripts/Managers/SaveManager/SaveManager.cs
+++ Assets/_Complete-Game/Scripts/Managers/SaveManager/SaveManager.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace CompleteProject
@@ -8,6 +10,15 @@ namespace CompleteProject
     {
         [SerializeField]
         private EnemyManager enemyManager;
+
+        private string savePath;
+
+        void Awake()
+        {
+            // TODO create constant for the save file name
+            savePath = Application.persistentDataPath + "/save.dat";
+            Debug.Log(savePath);
+        }
 
         // Use this for initialization
         void Start()
@@ -24,6 +35,31 @@ namespace CompleteProject
         public void Save()
         {
             List<GameObject> enemies = enemyManager.GetAllAliveEnemies();
+
+            // create save data object
+            PlayerData playerData = CreatePlayerData();
+            GameSave gameSaveData = new GameSave
+            {
+                PlayerData = playerData
+            };
+
+            SerializeSaveData(gameSaveData);
+        }
+
+        private PlayerData CreatePlayerData()
+        {
+            // TODO Implement
+            return new PlayerData {Health = 100, Score = 200};
+        }
+
+        private void SerializeSaveData(GameSave gameSaveData)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            FileStream fileStream = File.Create(savePath);
+            binaryFormatter.Serialize(fileStream, gameSaveData);
+            fileStream.Close();
+
+            Debug.Log("SerializeSaveData()");
         }
 
         public void Load()
