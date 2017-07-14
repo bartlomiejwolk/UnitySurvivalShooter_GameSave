5880c6b2d1f71c2dcd36710b3edfc1eef13e3647
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2017-07-14 08:48:16 +0200

    Create SaveManager and SaveManagerEditor
    - draw save and load buttons

---
+++ Assets/_Complete-Game/Scripts/Managers/SaveManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompleteProject
+{
+    public class SaveManager : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+
+        public void Load()
+        {
+
+        }
+    }
+
+}
